e7a043f8d0ec63417ba09a6cfb5887d31c144120
Reynhart <reynhart_i_malingkas@telkomsel.co.id>
2024-11-04 17:00:28 +0700

    update login screen

--- composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/App.kt
+++ composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/App.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -23,6 +27,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,6 +36,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import baksotsel.composeapp.generated.resources.Res
 import baksotsel.composeapp.generated.resources.compose_multiplatform
+import baksotsel.composeapp.generated.resources.logo
 import org.reynhart.baksotsel.ui.theme.backgroundDark
 import org.reynhart.baksotsel.ui.theme.backgroundLight
 import org.reynhart.baksotsel.ui.theme.primaryLight
@@ -41,10 +47,20 @@ import org.reynhart.baksotsel.widgets.BaksoTextField
 @Preview
 fun App() {
     var nameTxt by remember { mutableStateOf("") }
+    var dropdownValue by remember { mutableStateOf("") }
+    var agreementChecked by remember { mutableStateOf(false) }
 
     MaterialTheme {
         Column (
-            modifier = Modifier.fillMaxHeight().padding(24.dp)) {
+            modifier = Modifier.fillMaxHeight().padding(24.dp),
+            verticalArrangement = Arrangement.Bottom
+            ) {
+            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(resource = Res.drawable.logo),
+                    contentDescription = "Logo")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
             Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
                 Text("Verifikasi", fontSize = 24.sp, fontWeight = FontWeight.Bold)
             }
@@ -52,22 +68,48 @@ fun App() {
                 Text("Masukan nama dan role Anda di bawah ini", fontSize = 14.sp)
             }
 
-            Column(modifier = Modifier.padding(16.dp)) {
-                BaksoTextField(
-                    label = "Nama",
-                    value = nameTxt,
-                    placeholder = "Masukan nama",
-                    onValueChange = { nameTxt = it },
-                )
-                BaksoDropdown(
-                    label = "Role",
-                )
-                Button(onClick = {},
-                    modifier = Modifier.fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(containerColor = primaryLight)
-                ){
-                    Text("Join")
+            Column(modifier = Modifier.padding(10.dp)
+                .background(color = Color(red = 108f, green = 122f, blue = 137f),
+                    shape = RoundedCornerShape(24.dp))) {
+                Column(modifier = Modifier.padding(14.dp)) {
+                    BaksoTextField(
+                        label = "Nama",
+                        value = nameTxt,
+                        placeholder = "Masukan nama",
+                        onValueChange = { nameTxt = it },
+                    )
+                    BaksoDropdown(
+                        label = "Role",
+                        placeholder = "Pilih Role",
+                        dropdownMap = mapOf("Tukang Bakso" to "tb", "Customer" to "c"),
+                        onDropdownChange = {dropdownValue=it}
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    HorizontalDivider(thickness = 2.dp)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        enabled = nameTxt.trim() != "" && dropdownValue != "" && agreementChecked,
+                        colors = ButtonDefaults.buttonColors(containerColor = primaryLight)
+                    ){
+                        Text("Join")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row {
+                        Checkbox(
+                            checked = agreementChecked,
+                            onCheckedChange = { agreementChecked = it },
+                            modifier = Modifier.padding(0.dp)
+                        )
+                        Text(
+                            "Dengan menggunakan aplikasi ini, Anda telah setuju untuk membagikan lokasi Anda ke tukang bakso keliling.",
+                            fontSize = 14.sp
+                        )
+                    }
                 }
+
+
+
             }
 
 
--- composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/widgets/BaksoDropdown.kt
+++ composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/widgets/BaksoDropdown.kt
@@ -1,6 +1,8 @@
 package org.reynhart.baksotsel.widgets
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
@@ -10,26 +12,70 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.reynhart.baksotsel.ui.theme.onPrimaryContainerLight
+import org.reynhart.baksotsel.ui.theme.primaryLight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaksoDropdown(
     label: String,
+    placeholder: String,
+    dropdownMap: Map<String, String>,
+    onDropdownChange: (String)-> Unit
 ){
-    var isShowBottomSheet = remember { mutableStateOf(false) }
-    Text(label, fontSize = 14.sp)
-    OutlinedButton(onClick = {}, modifier = Modifier.fillMaxWidth()){
-        Text("Pilih Role")
-    }
-    ModalBottomSheet(onDismissRequest = {} ){
+    var isShowBottomSheet by remember { mutableStateOf(false) }
+    var currentValue by remember{ mutableStateOf("") }
+    var currentLabel by remember{ mutableStateOf(placeholder) }
+    val modalSheetState = rememberModalBottomSheetState()
 
+    Text(label, fontSize = 14.sp)
+    OutlinedButton(onClick = {
+        isShowBottomSheet = true
+    }, modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = onPrimaryContainerLight,
+            contentColor = primaryLight)
+    ){
+        Text(currentLabel)
     }
+
+    if(isShowBottomSheet){
+        ModalBottomSheet(onDismissRequest = {
+            isShowBottomSheet=false
+        }, sheetState = modalSheetState ){
+            Column (modifier = Modifier.padding(16.dp)) {
+                dropdownMap.forEach { it ->
+                    OutlinedButton(onClick = {
+                        isShowBottomSheet = false
+                        currentValue = it.value
+                        currentLabel = it.key
+                        onDropdownChange(currentValue)
+                    },
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = onPrimaryContainerLight,
+                            contentColor = primaryLight)
+                    ){
+                        Text(it.key)
+                    }
+                }
+
+            }
+
+        }
+    }
+
 }
