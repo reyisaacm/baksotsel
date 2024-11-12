35c6088461a658cff3a3ae69c9259f5cd9922d39
Reynhart <reynhart_i_malingkas@telkomsel.co.id>
2024-11-12 16:59:53 +0700

    add di binding

--- composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/di/KoinConfig.kt
+++ composeApp/src/commonMain/kotlin/org/reynhart/baksotsel/di/KoinConfig.kt
@@ -1,10 +1,13 @@
 package org.reynhart.baksotsel.di
 
+import org.koin.dsl.bind
 import org.koin.dsl.koinApplication
 import org.koin.dsl.module
+import org.reynhart.baksotsel.data.interfaces.repository.IStorageRepository
 import org.reynhart.baksotsel.data.repository.StorageRepository
 
 val sharedModule= module {
     single {
-    }
+        StorageRepository(get())
+    }.bind<IStorageRepository>()
 }
