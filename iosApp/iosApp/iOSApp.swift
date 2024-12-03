39d7972fd4b71f39b4d579d4095ec665756b3a6e
Reynhart <lightning2006@gmail.com>
2024-12-03 19:37:07 +0700

    update location for ios devices

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import GoogleMaps //add import
 
 @main
 struct iOSApp: App {
+    init() {
+        GMSServices.provideAPIKey("")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
