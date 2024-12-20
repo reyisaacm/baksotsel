053af7a4cb80972266a3cf2a1e9169780be658d1
Reynhart <reynhart_i_malingkas@telkomsel.co.id>
2024-12-20 16:38:04 +0700

    add name label on customer marker

--- composeApp/src/androidMain/kotlin/org/reynhart/baksotsel/GoogleMapView.android.kt
+++ composeApp/src/androidMain/kotlin/org/reynhart/baksotsel/GoogleMapView.android.kt
@@ -5,10 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
@@ -35,9 +40,12 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Marker
@@ -87,21 +95,40 @@ actual fun GoogleMapView(
             )
 
             if(it.type == "c"){
+                var isClicked by remember { mutableStateOf(false) }
                 MarkerComposable(
-                    state = markerState
-                ){
-                    Column(modifier = Modifier.width(36.dp).height(36.dp).background(
-                        color = Color.Red,
-                        shape = CircleShape
-                    ), verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Icon(
-                            imageVector = Icons.Filled.Person,
-                            contentDescription = "Favorite Icon",
-                            tint = Color.White,
-                            modifier = Modifier.width(24.dp).height(24.dp)
-                        )
+                    keys = arrayOf(it.id),
+                    state = markerState,
+                    onClick = {
+                        isClicked = !isClicked
+                        isClicked
                     }
+                ){
+                    Row {
+                        Column(modifier = Modifier.width(36.dp).height(36.dp).background(
+                            color = Color.Red,
+                            shape = CircleShape
+                        ), verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                imageVector = Icons.Filled.Person,
+                                contentDescription = "Favorite Icon",
+                                tint = Color.White,
+                                modifier = Modifier.width(24.dp).height(24.dp)
+                            )
+                        }
+
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Column(modifier = Modifier.background(
+                                color = Color.White,
+                                shape = RoundedCornerShape(10.dp)
+                            ).padding(8.dp)) {
+                                Text(it.name)
+                            }
+
+
+                    }
+
 
                 }
             }
